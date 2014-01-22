e4c40903de2c08831ac51ba0331343897314c1b3
Bradley Allen <bradley.m.allen@me.com>
2014-01-22 22:54:37 +0000

    adding full_name to the validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
  has_many :friendships
  has_many :friends, through: :friendships
 
- validates_presence_of :provider, :uid, :username, :image_url
+ validates_presence_of :provider, :uid, :username, :image_url, :full_name
 end
