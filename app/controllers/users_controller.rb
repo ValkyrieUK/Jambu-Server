8835f068c4ea9795bd4b253351771371543a074a
Bradley Allen <bradley.m.allen@me.com>
2014-01-22 15:09:08 +0000

    tidied API, now working delted views and assets as Im just building a API not a application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    User.all
+  end
+
+  def create
+    User.new(params[:user])
+  end
+
+  def show
+    User.find(parmas[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+  end 
 end
