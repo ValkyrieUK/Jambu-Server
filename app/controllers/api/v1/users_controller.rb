8a23c0dfc9cc85ee60db55a7288a74e83d2adf2b
Bradley Allen <bradley.m.allen@me.com>
2014-01-25 19:20:26 +0000

    adding =

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,12 +8,12 @@ module Api
       end
 
       def show
-        respond_with User.search(params[:username])
+        respond_with User.find_by_uid(params[:uid])
       end
 
       # Not so sure about save here..
       def create
-        User.new(user_params).save, location: nil
+        User.new(user_params).save, location = nil
       end
 
       def update
