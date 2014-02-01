0ae5fd2a099022b416f2b41961fdfb3c8edb78e9
Bradley Allen <bradley.m.allen@me.com>
2014-02-01 18:05:31 +0000

    friends working building by record ID

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
 
   validates :provider, :uid, :username, :image_url, :full_name, :colour, presence: true
   validates :uid, uniqueness: true
