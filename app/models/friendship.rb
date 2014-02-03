6f49b4cb69571e7b2867f6c56dbc7d947384ae0f
Bradley Allen <bradley.m.allen@me.com>
2014-02-03 22:34:45 +0000

    new dupe checking

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,12 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  before_save :check_for_duplicates?
 
-  validates :user_id, :friend_id, uniqueness: true
+
+  def check_for_duplicates?
+    if Friendship.find(:all, :conditions => ["user_id = ? and friend_id = ?", self.user_id, self.friend_id]).any?
+      raise 'Duplicate'
+    end
+  end
 end
