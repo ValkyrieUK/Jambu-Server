374822eac30b83fea1a94395789ac248e8775500
Bradley Allen <bradley.m.allen@me.com>
2014-02-03 22:10:56 +0000

    trying some fancy validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,5 +3,5 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  # validates :friend_id, uniqueness: true
+  validates :user_id, :friend_id, uniqueness: true
 end
