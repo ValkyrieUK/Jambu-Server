c4c055324e502fb820f521a9cf977abebd3a33cc
Bradley Allen <bradley.m.allen@me.com>
2014-02-04 21:59:54 +0000

    adding thumbnail

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
 
-  validates :provider, :uid, :username, :image_url, :full_name, :colour, presence: true
+  validates :provider, :uid, :username, :image_url, :image_thumbnail, :full_name, :colour, presence: true
   validates :uid, uniqueness: true
   validates :provider, format: /\A(twitter)\Z/
 
