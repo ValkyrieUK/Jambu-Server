15447e9c60c32506256109620038adddd1442c66
Bradley Allen <bradley.m.allen@me.com>
2014-02-04 22:11:27 +0000

    removed forgery protection

--- db/schema.rb
+++ db/schema.rb
@@ -50,10 +50,10 @@ ActiveRecord::Schema.define(version: 20140204215729) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "friendships", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
