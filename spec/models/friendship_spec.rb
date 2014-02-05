ce140e16dab780fcef4289467745392014554397
Bradley Allen <bradley.m.allen@me.com>
2014-02-05 21:22:17 +0000

    merging event model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe Friendship do
+  before :each do
+    user = User.create(
+      provider: 'twitter', username: 'BillyBob', 
+      image_url: 'http://...', image_thumbnail: 'http:..', 
+      full_name: 'Bill Bob', uid: '1234', colour: 'blue')
+    friend = User.create(
+      provider: 'twitter', username: 'Jimmy',
+      image_url: 'http://...', image_thumbnail: 'http:..',
+      full_name: 'Bill Bob', uid: '928',
+      colour: 'blue')
+  end
 
   it 'should create a friendship' do
     friendship = Friendship.create(user_id: 1, friend_id: 2)
