6755f9175b26f2d62def15a2d0c1c631c5ba3ed9
Bradley Allen <bradley.m.allen@me.com>
2014-02-05 20:03:07 +0000

    fixing duplicate finder method

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,9 +4,7 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, class_name: 'User'
   before_save :check_for_duplicates?
 
-  def check_for_duplicates?(find_options = {})
-    if Friendship.where(find_options, conditions: ['user_id = ? and friend_id = ?', user_id, friend_id]).any?
-      raise 'Duplicate'
-    end
+  def check_for_duplicates?
+    fail 'Duplicate'  if Friendship.find(:all, conditions: ['user_id = ? and friend_id = ?', user_id, friend_id]).any?
   end
 end
