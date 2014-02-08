c67eefec2485369495b3ceb4f0eaf5eb99999b48
Bradley Allen <bradley.m.allen@me.com>
2014-02-08 20:15:31 +0000

    adding loggin to dev enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Server::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.logger = Logger.new(STDOUT)
-
+  config.log_level = :info
   # Print deprecation notices to the Rails logger.
   # config.active_support.deprecation = :log
 
