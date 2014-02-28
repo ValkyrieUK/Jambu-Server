b5d32adf9a7c0f2c65222f39d7ffa5a6bdca4dc1
Bradley Allen <bradley.m.allen@me.com>
2014-02-28 16:43:52 +0000

    send notification to user instead of token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery
-  http_basic_authenticate_with name: 'xaler', password: 'n[Q3}UK2>dM4Y7', if: :admin_controller?
-
-  def admin_controller?
-    self.class < ActiveAdmin::BaseController
-  end
 end
