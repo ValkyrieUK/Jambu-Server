cb8f6d237bef502d3ee7eee4876b9c1213428e29
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 02:03:46 +0000

    giving the time of a event a default value

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 # Event Model
 class Event < ActiveRecord::Base
   after_create :track
+  after_initialize :nullify_time
 
   belongs_to :user
 
@@ -11,6 +12,10 @@ class Event < ActiveRecord::Base
 
   validates :title, :user_id, presence: true
   validates :title, uniqueness: { scope: :user_id }
+
+  def nullify_time
+    self.time_of_event  ||= 'none'
+  end
 
   def track
     Activity.create(user_id: user_id, action: 'event created', name: title)
--- config/initializers/polling.rb
+++ config/initializers/polling.rb
@@ -6,7 +6,7 @@ scheduler.every '1s' do
     puts Event.where(['time_of_event < ?', now]).inspect
     event = Event.where(['time_of_event < ?', now]).each
     Event.where(['time_of_event < ?', now]).each do |e|
-      e.update(time_of_event: 'in progress...')
+      e.update(time_of_event: 'in progress or over')
       e.attending_users.each do |i|
         APNS.send_notification(i.device_token, "#{e.title} will begin in 60 seconds") unless i.device_token == 'NONE' || i.device_token.nil?
       end
