1153279e9eb29333542a036f17eb5f694d0e839e
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 01:46:10 +0000

    moved in progress up higher to stop duplicate apns

--- config/initializers/polling.rb
+++ config/initializers/polling.rb
@@ -6,10 +6,10 @@ scheduler.every '1s' do
     puts Event.where(['time_of_event < ?', now]).inspect
     event = Event.where(['time_of_event < ?', now]).each
     Event.where(['time_of_event < ?', now]).each do |e|
+      e.update(time_of_event: 'in progress...')
       e.attending_users.each do |i|
         APNS.send_notification(i.device_token, "#{e.title} will begin in 60 seconds") unless i.device_token == 'NONE' || i.device_token.nil?
       end
-      e.update(time_of_event: 'in progress...')
     end
   end
 end
