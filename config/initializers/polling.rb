10613737df4f17e85c58633d8f268ab5cee65826
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 01:40:09 +0000

    update poll for less than

--- config/initializers/polling.rb
+++ config/initializers/polling.rb
@@ -2,13 +2,14 @@ scheduler = Rufus::Scheduler.new
 scheduler.every '1s' do
   now = (Time.now.to_i + 60).to_s
   puts Time.now.to_i
-  if Event.where(time_of_event: now).any?
-    puts Event.where(time_of_event: Time.now.to_s).inspect
-    event = Event.where(time_of_event: now).each
-    Event.where(time_of_event: now).each do |e|
+  if Event.where(['time_of_event < ?', now])
+    puts Event.where(['time_of_event < ?', now]).inspect
+    event = Event.where(['time_of_event < ?', now]).each
+    Event.where(['time_of_event < ?', now]).each do |e|
       e.attending_users.each do |i|
         APNS.send_notification(i.device_token, "#{e.title} will begin in 60 seconds") unless i.device_token == 'NONE' || i.device_token.nil?
       end
+      e.update(time_of_event: 'in progress...')
     end
   end
 end
