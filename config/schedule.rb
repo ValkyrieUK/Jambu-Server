1c0f9208ded44584062365d1136c061403d6ed84
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 17:08:45 +0000

    just making things up now

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,9 +18,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-
 every 1.minutes do
   rake "apns:push"
 end
