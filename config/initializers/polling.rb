5666e3c635e50d90180db8422bfd57b5d91be8b1
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 13:20:50 +0000

    polling every 10 mins

--- config/initializers/polling.rb
+++ config/initializers/polling.rb
@@ -1,16 +1,15 @@
 scheduler = Rufus::Scheduler.new
-scheduler.every '7s' do
+scheduler.every '10m' do
   now = (Time.now.to_i + 60).to_s
   puts Time.now.to_i
   if Event.where(['time_of_event < ?', now])
     sleep 1
-    puts Event.where(['time_of_event < ?', now]).inspect
     event = Event.where(['time_of_event < ?', now]).each
     Event.where(['time_of_event < ?', now]).each do |e|
       e.update(time_of_event: 'in progress or over')
       e.attending_users.each do |i|
         APNS.send_notification(i.device_token, "#{e.title} will begin in 60 seconds") unless i.device_token == 'NONE' || i.device_token.nil?
-        sleep 1
+        sleep 1 
       end
     end
   end
