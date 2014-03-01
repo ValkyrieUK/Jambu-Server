8fef3007cb0f1cbb245d9a26e6ec0271f0a0b373
Bradley Allen <bradley.m.allen@me.com>
2014-03-01 19:14:04 +0000

    idek

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 # set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-every 1.minutes do
+loop do
   rake "apns:push"
 end
