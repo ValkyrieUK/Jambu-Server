98adb3842983f6d3a69ad8cc38f875c73aeb46ba
Bradley Allen <bradley.m.allen@me.com>
2014-03-02 21:51:34 +0000

    moved note to its own class

---
+++ lib/support/Note.rb
@@ -0,0 +1,21 @@
+include ActionView::Helpers::DateHelper
+
+class Note
+
+  def self.ping
+    now = (Time.now.to_i + 900).to_s
+    if Event.where(['time_of_event < ?', now])
+      Event.where(['time_of_event < ?', now]).each do |e|
+        time_until = distance_of_time_in_words(now.to_i, e.time_of_event.to_i)
+        e.attendees.each do |i|
+          user = User.find(i.user_id)
+          APNS.send_notification(user.device_token, "#{e.title} will be starting in #{time_until}!") unless user.device_token == 'NONE' || user.device_token.nil? || i.going? == false
+          sleep 1
+        end
+        e.update(time_of_event: 'in progress or over')
+      end
+    end
+    sleep 3
+  end
+
+end
