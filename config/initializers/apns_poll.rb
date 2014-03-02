01ddf5ea5643246155b009a5e3c0c808d7d05ce2
Bradley Allen <bradley.m.allen@me.com>
2014-03-02 20:20:53 +0000

    puts time until event into note

--- config/initializers/apns_poll.rb
+++ config/initializers/apns_poll.rb
@@ -1,18 +1,18 @@
-# include ActionView::Helpers::DateHelper
+include ActionView::Helpers::DateHelper
 Thread.new do
   while true do
     now = (Time.now.to_i + 900).to_s
     if Event.where(['time_of_event < ?', now])
       Event.where(['time_of_event < ?', now]).each do |e|
+        time_until = distance_of_time_in_words(now.to_i, e.time_of_event.to_i)
         e.attendees.each do |i|
           user = User.find(i.user_id)
-        # time_until = distance_of_time_in_words(now.to_i, e.time_of_event)
-          APNS.send_notification(user.device_token, "#{e.title} will be starting soon!") unless user.device_token == 'NONE' || user.device_token.nil? || i.going? == false
+          APNS.send_notification(user.device_token, "#{e.title} will be starting in #{time_until}!") unless user.device_token == 'NONE' || user.device_token.nil? || i.going? == false
           sleep 1
         end
         e.update(time_of_event: 'in progress or over')
       end
     end
-    sleep 15
+    sleep 1
   end
 end
