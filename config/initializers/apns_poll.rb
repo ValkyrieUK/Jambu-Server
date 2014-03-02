bcb1dfb0ba454317e6ea721da673ebe943022b7b
Bradley Allen <bradley.m.allen@me.com>
2014-03-02 01:45:19 +0000

    poll every 3 mins, sending push 15 erly

--- config/initializers/apns_poll.rb
+++ config/initializers/apns_poll.rb
@@ -1,17 +1,17 @@
 Thread.new do
   while true do
     puts Time.now.to_i
-    now = (Time.now.to_i - 60).to_s
+    now = (Time.now.to_i + 900).to_s
     if Event.where(['time_of_event < ?', now])
       event = Event.where(['time_of_event < ?', now]).each
       Event.where(['time_of_event < ?', now]).each do |e|
         e.update(time_of_event: 'in progress or over')
         e.attending_users.each do |i|
-        APNS.send_notification(i.device_token, "#{e.title} will be starting soon!") unless i.device_token == 'NONE' || i.device_token.nil?
+          APNS.send_notification(i.device_token, "#{e.title} will be starting soon!") unless i.device_token == 'NONE' || i.device_token.nil?
           sleep 1 
         end
       end
     end
-    sleep 20
+    sleep 180
   end
 end
