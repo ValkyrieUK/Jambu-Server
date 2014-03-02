aab89a104b5bef0af2bcea510fa5db89ed205ef0
Bradley Allen <bradley.m.allen@me.com>
2014-03-02 02:06:29 +0000

    time of event in note

--- config/initializers/apns_poll.rb
+++ config/initializers/apns_poll.rb
@@ -5,11 +5,11 @@ Thread.new do
     if Event.where(['time_of_event < ?', now])
       event = Event.where(['time_of_event < ?', now]).each
       Event.where(['time_of_event < ?', now]).each do |e|
-        e.update(time_of_event: 'in progress or over')
         e.attending_users.each do |i|
-          APNS.send_notification(i.device_token, "#{e.title} will be starting soon!") unless i.device_token == 'NONE' || i.device_token.nil?
+          APNS.send_notification(i.device_token, "#{e.title} will be starting in #{distance_of_time_in_words(now, e.time_of_event)}!") unless i.device_token == 'NONE' || i.device_token.nil?
           sleep 1 
         end
+        e.update(time_of_event: 'in progress or over')
       end
     end
     sleep 180
