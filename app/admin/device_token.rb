736f5f917cba0b28b609828def7ad2861901e0a8
Bradley Allen <bradley.m.allen@me.com>
2014-03-05 23:21:25 +0000

    updated admin panel for device_tokens

--- app/admin/device_token.rb
+++ app/admin/device_token.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register DeviceToken do
   permit_params :user_id, :os, :token
+
+  index do
+    selectable_column
+    column :user_id
+    column :os
+    default_actions
+  end
 end
