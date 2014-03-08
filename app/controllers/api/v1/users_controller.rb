f828bc2423be8a1b21769127ebb5086d96697e5f
Bradley Allen <bradley.m.allen@me.com>
2014-03-08 13:34:26 +0000

    learn to spell and use git 10/10 would watch again

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -36,7 +36,7 @@ module Api
       def create
         user = User.new(user_params)
         if user.save!
-          render json: { sucess: true }
+          render json: { success: true }
         else
           render json: { errors: user.errors.full_messages }
         end
