4a229bb56263564aef25b3d922f0ea1b0fee4ce9
Bradley Allen <bradley.m.allen@me.com>
2014-03-12 20:20:28 +0000

    loading more things into RAM and Cache

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,19 +12,22 @@ module Api
 
       def show
         user = User.find_by_uid(params[:uid])
+        join_event = Event.joins(:attending_users).merge(Attendee.where(user_id: user.id, going?: true))
         render json: { error: 'User does not exist!' } if user.nil?
         return if user.nil?
-        next_event = Event.joins(:attending_users).merge(Attendee.where(user_id: user.id, going?: true)).order(:time_of_event)
+        next_event = join_event.order(:time_of_event)
         friend_user_id = User.find_by_uid(params[:requestor]).id if params[:requestor]
         friendship_id = Friendship.where(user_id: friend_user_id, friend_id: user.id)
+
+        # Begin Creating the Hash to return
         user_hash = {
           user: user,
           counts: {
             friends: user.friends.count ,
             added_as_friend: user.inverse_friendships.count,
             events_created: user.events.count,
-            events_attended: Event.joins(:attending_users).merge(Attendee.where(user_id: user.id, going?: true)).count,
-            event_invites_pending: Attendee.where(user_id: user.id, going?: nil).count }
+            events_attended: join_event.count,
+            event_invites_pending: join_event.count }
           }
         if friendship_id.any? && friend_user_id
           user_hash.merge!(friend_id: friendship_id.last.id)
