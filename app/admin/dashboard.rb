3e6716e871833fa2613ed7d50f8a6657d6099262
Bradley Allen <bradley.m.allen@me.com>
2014-03-13 21:40:55 +0000

    added activitiess graph

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,6 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content do
-    render 'index_stat'
+    render 'stats'
   end
 end
