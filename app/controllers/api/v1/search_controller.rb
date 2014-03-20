520d9a41e0da017a4fcd19acec0ba6e2b72a28de
Bradley Allen <bradley.m.allen@me.com>
2014-03-20 19:11:07 +0000

    limit search querey size

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -12,6 +12,10 @@ module Api
           render json: { error: 'Serch Term is less than 3 Chrarcters' }
           return
         end
+        if term.length > 80
+          render json: { error: 'Serch Term is too long' }
+          return
+        end
         respond_with users: User.search(term).offset(params[:results]).limit(10)
       end
     end
