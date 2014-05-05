d3ce1f30a2f373cda9675e8b4fef1aacea65b523
Bradley Allen <bradley.m.allen@me.com>
2014-05-05 20:21:15 +0100

    limit 50, order desc, for owned events

--- app/controllers/api/v1/owned_events_controller.rb
+++ app/controllers/api/v1/owned_events_controller.rb
@@ -8,7 +8,7 @@ module Api
       def show
         user = User.find_by(uid: params[:id])
         return unless user
-        events = Event.where(user_id: user.id)
+        events = Event.where(user_id: user.id).order('created_at DESC').limit(50)
         render json: { events: events}
       end
     end
