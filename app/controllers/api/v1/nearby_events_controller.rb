de33939b999d3bdb03b83e6280f405a53a604609
Bradley Allen <bradley.m.allen@me.com>
2014-05-10 00:14:50 +0100

    near events no old

--- app/controllers/api/v1/nearby_events_controller.rb
+++ app/controllers/api/v1/nearby_events_controller.rb
@@ -6,7 +6,7 @@ module Api
     class NearbyEventsController < ApplicationController
       respond_to :json
       def nearby
-        render json: { events: Event.near([params[:lat], params[:long]], params[:distance]).where(privacy: 'public') }
+        render json: { events: Event.near([params[:lat], params[:long]], params[:distance]).where(privacy: 'public', canceled: 'false') }
       end
     end
   end
